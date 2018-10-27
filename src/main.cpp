fd99a664f084b8eacf085e5c34f5e6c6383c5aa2
marssys <gera.petrov@gmail.com>
2018-10-27 18:20:20 +0300

    code added to main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,15 @@
 #include <Arduino.h>
 
+int LED_SONOFF = 13;
+int BLINK_DURATION = 1000;
+
 void setup() {
-    // put your setup code here, to run once:
+    pinMode(LED_SONOFF, OUTPUT);
 }
 
 void loop() {
-    // put your main code here, to run repeatedly:
+    digitalWrite(LED_SONOFF, LOW); // LOW will turn on the LED
+    delay(BLINK_DURATION);
+    digitalWrite(LED_SONOFF, HIGH); // HIGH will turn off the LED
+    delay(BLINK_DURATION);
 }
